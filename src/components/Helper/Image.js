ef66288b400e33be62ebeddb93476297684807fe
paulo.azevedo <pauloemidioazevedo@gmail.com>
2021-08-09 16:41:44 -0300

    dogs : Minor Adjustments

--- src/components/Helper/Image.js
+++ src/components/Helper/Image.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-
-import styles from "./Image.module.css";
+import React from 'react';
+import styles from './Image.module.css';
 
 const Image = ({ alt, ...props }) => {
-  const [skeleton, setSkeleton] = useState(true);
+  const [skeleton, setSkeleton] = React.useState(true);
 
   function handleLoad({ target }) {
     setSkeleton(false);
@@ -13,8 +12,9 @@ const Image = ({ alt, ...props }) => {
   return (
     <div className={styles.wrapper}>
       {skeleton && <div className={styles.skeleton}></div>}
-      <img onLoad={handleLoad} className={styles.img} {...props} alt={alt} />
+      <img onLoad={handleLoad} className={styles.img} alt={alt} {...props} />
     </div>
   );
 };
+
 export default Image;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import Head from "../components/Helper/Head"
 
 const Home = () => {
   return (
-    <section className="mainContainer">
+    <section className="container mainContainer">
       <Head title="Fotos" description="Home do site , com feed de fotos!"/>
       <Feed />
     </section>
